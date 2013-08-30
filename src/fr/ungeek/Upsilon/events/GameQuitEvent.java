145da178cd36f48a8a28cb068fb5ee42f0dd72d7
PunKeel <jesuis@ungeek.fr>
2013-08-30 11:37:40 +0200

    Mais mais :o

---
+++ src/fr/ungeek/Upsilon/events/GameQuitEvent.java
@@ -0,0 +1,10 @@
+package fr.ungeek.Upsilon.events;
+
+/**
+ * User: PunKeel
+ * Date: 5/12/13
+ * Time: 3:59 PM
+ * May be open-source & be sold (by PunKeel, of course !)
+ */
+public class GameQuitEvent {
+}
